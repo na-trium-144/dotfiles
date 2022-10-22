22f78ca7fd582285e93363a25124a025197f2d33
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-10-22 17:50:31 +0000

    pacmanに対応

--- dot_local/shell/executable_misc.sh
+++ dot_local/shell/executable_misc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-sudo apt-get install -y cargo micro peco mc tmux || brew install rust micro peco mc tmux
+if type apt-get; then
+	sudo apt-get install -y cargo micro peco mc tmux
+elif type pacman; then
+	sudo pacman -S cargo micro peco mc tmux
+elif type brew; then
+	brew install rust micro peco mc tmux
+fi
 cargo install fd-find git-delta hexyl
 curl https://pyenv.run | bash
 pip3 install pipenv
