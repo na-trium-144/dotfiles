f786dbd5aa569a91fcc60119595200e8bb2729d7
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-01-22 17:22:40 +0900

    extrakto追加

--- dot_local/shell/executable_misc.sh
+++ dot_local/shell/executable_misc.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+# pip, cargo > apt, pacman, brew > install script
 if type apt-get; then
-	sudo apt-get install -y cargo micro peco mc tmux
+	sudo apt-get install -y cargo micro peco mc tmux fzf
 elif type pacman; then
-	sudo pacman -S cargo micro peco mc tmux
+	sudo pacman -S cargo micro peco mc tmux fzf
 elif type brew; then
-	brew install rust micro peco mc tmux
+	brew install rust micro peco mc tmux fzf
 fi
 cargo install fd-find git-delta hexyl
 curl https://pyenv.run | bash
 pip3 install pipenv
+[ -d ~/.local/extrakto ] || git clone https://github.com/laktak/extrakto ~/.local/extrakto
