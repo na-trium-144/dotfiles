56f94f50e8fe12f7a4f9479307c3b18445b2d5a1
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-02-10 16:15:18 +0000

    poetry

--- utils/misc.sh
+++ utils/misc.sh
@@ -19,6 +19,7 @@ type cargo || curl https://sh.rustup.rs -sSf | sh
 cargo install fd-find git-delta hexyl
 type pyenv || curl https://pyenv.run | bash
 pip3 install pipenv
+type poetry || curl -sSL https://install.python-poetry.org | python3 -
 if ! [ -d ~/.tmux/plugins/tpm ]; then
 	mkdir -p ~/.tmux/plugins
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -28,3 +29,9 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install --lts
 npm install -g git-user-switch
+
+#completion
+rm ~/.bash_completion
+poetry completions bash >> ~/.bash_completion
+chezmoi completion bash >> ~/.bash_completion
+npm completion bash >> ~/.bash_completion
