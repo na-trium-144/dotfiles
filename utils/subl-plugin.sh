587b3084546f8284e694227f50d1021a9deb220c
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-02-21 01:18:21 +0900

    clang-format

--- utils/subl-plugin.sh
+++ utils/subl-plugin.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt install cppcheck tidy || brew install cppcheck
-pip3 install black pylint cmakelint
+pip3 install black pylint cmakelint clang-format
 cd ~/.config/sublime-text/Packages/User/formatter.assets/javascript && npm install
