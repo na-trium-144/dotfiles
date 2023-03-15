0349f265551a33b47c64708318d0d9bda9c04b3c
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-03-15 18:36:32 +0900

    Update msys2.sh

--- utils/msys2.sh
+++ utils/msys2.sh
@@ -2,13 +2,17 @@
 # pacman -S unzip <- chezmoi入れる前に必要
 # set HOME=C:\Users\hoge <- msys2の起動時に必要
 type git || pacman -S --noconfirm git
-# type micro || pacman -S --noconfirm micro
 type mc || pacman -S --noconfirm mc
 type tmux || pacman -S --noconfirm tmux
 type fzf || pacman -S --noconfirm mingw-w64-ucrt-x86_64-fzf
 type winpty || pacman -S --noconfirm winpty
 type fd || pacman -S --noconfirm mingw-w64-ucrt-x86_64-fd
-type micro || sh -c "curl https://getmic.ro | bash"
+if type choco; then
+	unset TEMP
+	unset TMP
+	type micro || choco install micro
+	type peco || choco install peco
+fi
 if ! [ -d ~/.tmux/plugins/tpm ]; then
 	mkdir -p ~/.tmux/plugins
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
