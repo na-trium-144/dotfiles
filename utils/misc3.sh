73ddfe3129068a3f3db62b15e04b04ce295f16b1
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-10-26 19:53:57 +0900

    add flake8

--- utils/misc3.sh
+++ utils/misc3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt install cppcheck tidy || brew install cppcheck
-pip3 install black pylint cmakelint clang-format mypy
+pip3 install black pylint cmakelint clang-format mypy flake8
 cd ~/.config/sublime-text/Packages/User/formatter.assets/javascript && npm install
