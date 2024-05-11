7ff3cb960e0e51ecc7707eb4a955a472d2ef9265
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-05-11 20:18:08 +0900

    ruby

--- utils/misc3.sh
+++ utils/misc3.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+# clangdはインストールしない
 if type apt-get; then
-	sudo apt-get install -y cppcheck tidy
+	sudo apt-get install -y cppcheck tidy ruby
 elif type brew; then
-	brew install cppcheck
+	brew install cppcheck ruby
 fi
 pip3 install black pylint cmakelint clang-format mypy flake8
 cd ~/.config/sublime-text/Packages/User/formatter.assets/javascript && npm install
