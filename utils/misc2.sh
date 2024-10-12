7adbcadbb5c81b93b796b62709cf48122cebeb46
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-10-12 22:17:28 +0900

    なんかnpmのパスが通ったり通らなかったりする

--- utils/misc2.sh
+++ utils/misc2.sh
@@ -30,13 +30,17 @@ pipx install poetry
 # curl -sSL https://install.python-poetry.org | python3 - --uninstall
 
 if uname | grep MINGW64 >/dev/null; then
+    export NVM_HOME="C:\ProgramData\nvm"
+    export NVM_SYMLINK="C:\Program Files\nodejs"
     if ! type nvm >/dev/null; then
         type node >/dev/null && sudo choco uninstall nodejs nodejs.install
         sudo choco install -y nvm
+        export PATH="$(cygpath -u "$NVM_HOME"):$(cygpath -u "$NVM_SYMLINK"):$PATH"
     fi
     for ver in $(cygpath -u "$NVM_HOME")/v*; do nvm uninstall $(echo $(basename $ver) | sed s/v//) || true; done
     nvm install lts
     nvm use lts
+    export PATH="$(cygpath -u "$NVM_HOME"):$(cygpath -u "$NVM_SYMLINK"):$PATH"
 else
     unset NVM_DIR
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
@@ -53,4 +57,4 @@ npm install -g git-user-switch
 [ -e ~/.bash_completion ] && rm ~/.bash_completion
 poetry completions bash >> ~/.bash_completion
 chezmoi completion bash >> ~/.bash_completion
-npm completion bash >> ~/.bash_completion
+npm completion bash >> ~/.bash_completion || true
