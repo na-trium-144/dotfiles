4d383df585bf38e496dd1d950ab1c385bc5c1665
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-10-15 02:21:39 +0900

    doxygenにbison

--- utils/source_builds.sh
+++ utils/source_builds.sh
@@ -12,10 +12,19 @@ else
 fi
 cargo install fd-find git-delta hexyl
 
+if uname | grep MINGW64 >/dev/null; then
+    # for doxygen
+    type flex || pacman -S --noconfirm flex
+    type bison || pacman -S --noconfirm bison
+elif type brew; then
+    brew install bison m4
+    export PATH="$(brew --prefix bison)/bin:$(brew --prefix m4)/bin:$PATH"
+fi
 pushd $(dirname $0)/doxygen
-cmake -Bbuild -DCMAKE_INSTALL_PREFIX=$HOME/.local
+cmake -Bbuild -DCMAKE_INSTALL_PREFIX=$HOME/.local $flags
 cmake --build build --target install
 popd
+unset flags
 
 pushd $(dirname $0)/json-tui
 if uname | grep MINGW64 >/dev/null; then
