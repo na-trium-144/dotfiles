e718e4767db43f37d723696cb9d5d99f85d44cd6
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-10-29 19:08:17 +0900

    windowsでtbの動作を修正

--- scripts/tb.sh
+++ scripts/tb.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-_uname=$(uname) # Darwin, Linux, MINGW64_NT_*
 if [[ $# > 0 ]]; then
 	if [[ "$1" = "-" ]]; then
 		input="$(cat)"
 	else
 		input="$@"
 	fi
-	[[ "${_uname}" = "Linux" ]] && echo -n "$input" | xclip -selection clipboard -i
-	[[ "${_uname}" = "Darwin" ]] && echo -n "$input" | pbcopy
-	[[ "${_uname}" = "MINGW64_NT" ]] && echo -n "$input" | sh -c "iconv -f utf-8 -t cp932 | clip"
+	if [[ "${_uname}" = "Linux" ]]; then
+		echo -n "$input" | xclip -selection clipboard -i
+	elif [[ "${_uname}" = "Darwin" ]]; then
+		echo -n "$input" | pbcopy
+	elif [[ "${_uname}" = "MINGW64_NT" ]]; then
+		echo -n "$input" | sh -c "iconv -f utf-8 -t cp932 | clip"
+	else
+		echo "_uname is not set"
+	fi
 	echo -n "$input" | tmux load-buffer -
 else
 	tmux save-buffer -
