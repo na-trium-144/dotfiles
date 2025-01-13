89481526da6b59f392fa00a8d078729c95b49ea7
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-01-14 02:18:02 +0900

    pythonの依存ライブラリ修正

--- utils/pyenv.sh
+++ utils/pyenv.sh
@@ -13,7 +13,22 @@ if uname | grep MINGW64 >/dev/null; then
     rm install-pyenv-win.ps1
 else
     type pyenv || curl https://pyenv.run | bash
-    $(dirname $0)/python_dep.sh || true
+    # no local brew alias here
+    # https://devguide.python.org/getting-started/setup-building/index.html#linux
+    # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+    if type brew; then
+        brew install tcl-tk readline ncurses
+    elif type pacman; then
+        sudo pacman -S --noconfirm --needed base-devel openssl zlib xz tk
+    elif type apt-get; then
+        type sudo && sudo=sudo
+        $sudo apt-get install -y build-essential gdb lcov pkg-config \
+          libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
+          libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
+          lzma lzma-dev tk-dev uuid-dev zlib1g-dev \
+          || true
+        # sudoが使えない環境は、無視
+    fi
 fi
 source $_chezmoi_root/scripts/load_pyenv.sh
 pyenv update
