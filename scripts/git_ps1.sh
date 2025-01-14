fdd0f277828bd8abefb28acf20baeeba7289a3ab
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-01-14 16:57:23 +0900

    add nix-profile path

--- scripts/git_ps1.sh
+++ scripts/git_ps1.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-for git_prompt_path in /usr/share/git/completion/git-prompt.sh /etc/bash_completion.d/git-prompt ~/.git-prompt.sh; do
+for git_prompt_path in \
+    /usr/share/git/completion/git-prompt.sh \
+    /etc/bash_completion.d/git-prompt \
+    ~/.nix-profile/share/git/completion/git-prompt.sh \
+    ~/.git-prompt.sh \
+; do
     if [[ -e $git_prompt_path ]]; then
         source $git_prompt_path
         break
@@ -10,4 +15,4 @@ GIT_PS1_SHOWDIRTYSTATE=true
 GIT_PS1_SHOWSTASHSTATE=true
 GIT_PS1_SHOWUPSTREAM="verbose"
 
-__git_ps1 | sed "s/(\\(.*\\))/(\\1 @$(git config user.name))/"
+if type __git_ps1 >/dev/null 2>&1 ; then __git_ps1 | sed "s/(\\(.*\\))/(\\1 @$(git config user.name))/"; fi
