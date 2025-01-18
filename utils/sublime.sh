c87629c3c687a26618e04bb65cd21896a2eb293f
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-01-19 02:04:51 +0900

    sublimeで使うLANGを修正

--- utils/sublime.sh
+++ utils/sublime.sh
@@ -7,7 +7,8 @@ if ! type subl; then
     curl -L https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
     echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
     sudo apt-get update
-    sudo apt-get install -y sublime-text sublime-merge libgl1
+    sudo apt-get install -y sublime-text sublime-merge libgl1 fontconfig language-pack-ja-base language-pack-ja
+    # set LANG=ja_JP.utf-8 later
 fi
 hash -r
 type subl
