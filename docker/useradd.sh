48b8e38b555e602fe2a8354216db6b3c51125905
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-09-22 15:02:20 +0900

    uidが存在した場合消す

--- docker/useradd.sh
+++ docker/useradd.sh
@@ -1,4 +1,7 @@
 set -e
+if id $BASE_UID; then
+    userdel $(id -nu $BASE_UID)
+fi
 useradd -u $BASE_UID -d $BASE_HOME -o -m -s/bin/bash $BASE_USER
 echo $BASE_USER:passwd | chpasswd
 groupmod -g $BASE_GID $BASE_USER || true
